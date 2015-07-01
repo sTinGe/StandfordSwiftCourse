ebbcaad91d60ce94f9d57fa1cd525c3d25a3d4d8
sTinGe <stinge62580@gmail.com>
2015-07-01 12:27:25 +0800

    first commit

---
+++ StandfordSwift/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  StandfordSwift
+//
+//  Created by sTinGe Su on 2015/7/1.
+//  Copyright (c) 2015年 sTinGe Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var display: UILabel!
+    
+}
+
