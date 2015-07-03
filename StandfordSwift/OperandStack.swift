516ad79e46419c7b7d72679e7fdf1adca28e58dd
sTinGe <stinge62580@gmail.com>
2015-07-03 14:21:23 +0800

    applying MVC

--- StandfordSwift/OperandStack.swift
+++ StandfordSwift/OperandStack.swift
@@ -8,38 +8,83 @@
 
 import Foundation
 
-class OperandStack {
-  var stack: Array<Double> = []
-  var displayValue: Double = 0
+class OperandStack{
+  // Printable is protocol
+  private enum Op {
+    case Operand(Double)
+    case UnaryOperation(String, Double -> Double)
+    case BinaryOperation(String, (Double, Double) -> Double)
   
-  func store(value: Double) {
-    displayValue = value
-  }
-  func push() {
-    stack.append(displayValue)
-  }
-  
-  func pop() -> Double {
-    return stack.removeLast()
-  }
-  
-  func peek() -> Double {
-    return stack[stack.count-1]
-  }
-  
-  func size() -> Int {
-    return stack.count
-  }
-  
-  func isEmpty() -> Bool {
-    if stack.count == 0 {
-      return true
-    } else {
-      return false
+    var description: String {
+      get {
+        switch self {
+        case .Operand(let operand):
+          return "\(operand)"
+        case .UnaryOperation(let symbol, _):
+          return symbol
+        case .BinaryOperation(let symbol, _):
+          return symbol
+        }
+      }
     }
   }
   
-  func traversal() -> Array<Double>{
-    return stack
+  
+  private var opStack = [Op]()
+  private var knownOps = [String:Op]()
+  
+  init() {
+    func learnOp(op: Op) {
+      knownOps[op.description] = op
+    }
+    
+    knownOps["×"] = Op.BinaryOperation("×") { $0 * $1 }
+    knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+    knownOps["+"] = Op.BinaryOperation("+", +)
+    knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+    knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    
+  }
+  private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+    if !ops.isEmpty {
+      var remainingOps = ops
+      let op = remainingOps.removeLast()
+      switch op {
+      case .Operand(let operand):
+        return (operand, remainingOps)
+      case .UnaryOperation(_, let operation):
+        let operandEvaluation = evaluate(remainingOps)
+        if let operand = operandEvaluation.result {
+          return (operation(operand), operandEvaluation.remainingOps)
+        }
+      case .BinaryOperation(_, let operation):
+        let op1Evaluation = evaluate(remainingOps)
+        if let operand1 = op1Evaluation.result {
+          let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+          if let operand2 = op2Evaluation.result{
+            return (operation(operand1, operand2), op2Evaluation.remainingOps)
+          }
+        }
+      }
+    }
+    return (nil, ops)
+  }
+  func evaluate() -> Double? {
+      let (result, remainder) = evaluate(opStack)
+    println("\(opStack) = \(result) with \(remainder) left over")
+      return result
+  }
+//
+  func push(operand: Double) -> Double? {
+    opStack.append(Op.Operand(operand))
+    return evaluate()
+  }
+  
+  func peformOperation(symbol: String) -> Double? {
+    // "Optional" Op
+    if let operation = knownOps[symbol] {
+      opStack.append(operation)
+    }
+    return evaluate()
   }
 }
--- StandfordSwift/ViewController.swift
+++ StandfordSwift/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController
   
   @IBOutlet weak var display: UILabel!
   var userIsInput:Bool = false
-  var operand = OperandStack()
+  var operandStack = OperandStack()
   
   
   @IBAction func appendDigit(sender: UIButton) {
@@ -27,42 +27,28 @@ class ViewController: UIViewController
   }
   
   @IBAction func operate(sender: UIButton) {
-    let operation = sender.currentTitle!
     if userIsInput {
       enter()
     }
     
-    switch operation {
-    case "✕": performOperation{ $0 * $1 }
-    case "÷": performOperation{ $1 / $0 }
-    case "+": performOperation{ $0 + $1 }
-    case "−": performOperation{ $1 - $0 }
-    case "√": performOperation{ sqrt($0) }
-    default:
-      break
+    if let operation = sender.currentTitle {
+      if let result = operandStack.peformOperation(operation) {
+        displayValue = result
+      } else {
+        displayValue = 0
+      }
     }
-  }
-  
-  func performOperation(operation: (Double, Double) -> Double) {
-    if operand.size() >= 2 {
-      displayValue = operation(operand.pop(), operand.pop())
-      enter()
-    }
-  }
-  
-  private func performOperation(operation: Double -> Double) {
-    if operand.size() >= 1 {
-      displayValue = operation(operand.pop())
-      enter()
-    }
+    
   }
   
   // put the number into stack
   @IBAction func enter() {
-    operand.store(transToDouble(display.text!))
     userIsInput = false
-    operand.push()
-    println("\(operand.traversal())")
+    if let result = operandStack.push(displayValue) {
+      displayValue = result
+    } else {
+      displayValue = 0
+    }
   }
   
   // properties
