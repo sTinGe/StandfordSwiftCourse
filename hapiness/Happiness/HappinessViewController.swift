69b4a1bcfcea269c69f7381ff721aca80f1686fd
sTinGe <stinge62580@gmail.com>
2015-07-12 20:16:21 +0800

    updating Happiness

--- hapiness/Happiness/HappinessViewController.swift
+++ hapiness/Happiness/HappinessViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class HappinessViewController: UIViewController {
-
-    
+  var happiness: Int = 50 { // 0 = very sad
+    didSet {
+      happiness = min(max(happiness, 0), 100)
+      println("happiness = \(happiness)")
+      updateUI()
+    }
+  }
+  func updateUI() {
+  }
 }
